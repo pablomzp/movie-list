a93637c49a0a896ab218499b111801b3d1c2c22c
pabloms <pablomendezsuarez96@gmail.com>
2021-08-27 20:48:51 +0200

    FEAT: Services added

---
+++ src/app/shared/services/movie.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(private http: HttpClient) { }
+
+  // API KEY: 64fd869f0ffe02ff4690ccd8257a48fc
+
+  getPopularMovies() {
+    return this.http.get('https://api.themoviedb.org/3/movie/popular?api_key=64fd869f0ffe02ff4690ccd8257a48fc');
+  }
+
+  getMovie(searchValue) {
+    return this.http.get('https://api.themoviedb.org/3/search/movie?api_key=64fd869f0ffe02ff4690ccd8257a48fc&query=' + searchValue);
+  }
+
+}
